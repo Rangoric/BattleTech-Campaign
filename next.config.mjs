5a6ad401c8d126558087a5b8405c7766cb1b57aa
Wilson Gearld Mead III <kem@rangoric.com>
2024-09-12 19:14:49 -0400

    Trying to fix build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  images: { unoptimized: true },
+  experimental: { images: { unoptimized: true } },
+  basePath: "/BattleTech-Campaign",
 };
 
 export default nextConfig;
