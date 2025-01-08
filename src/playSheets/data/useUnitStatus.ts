87237c6f424707301d8b4343c104aceb2607c935
Wilson Gearld Mead III <kem@rangoric.com>
2025-01-08 01:09:29 -0500

    Update useUnitStatus.ts

--- src/playSheets/data/useUnitStatus.ts
+++ src/playSheets/data/useUnitStatus.ts
@@ -3,10 +3,10 @@ import { IPlaySheetStatus } from "./IPlaySheetStatus";
 export const useUnitStatus = (
   unit: string
 ): [IPlaySheetStatus, (x: IPlaySheetStatus) => void] => {
-  const unitStatusString = localStorage.getItem(unit) ?? "{}";
+  const unitStatusString = window.localStorage.getItem(unit) ?? "{}";
   const unitStatus = JSON.parse(unitStatusString) as IPlaySheetStatus;
   const setUnitStatus = (status: IPlaySheetStatus) => {
-    localStorage.setItem(unit, JSON.stringify(status));
+    window.localStorage.setItem(unit, JSON.stringify(status));
   };
   return [unitStatus, setUnitStatus] as const;
 };
