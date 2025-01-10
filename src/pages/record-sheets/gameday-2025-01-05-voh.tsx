773e6d02de553d7be7b3ca073220f3952ef1cf50
Wilson Gearld Mead III <kem@rangoric.com>
2025-01-10 08:53:52 -0500

    Update gameday-2025-01-05-voh.tsx

--- src/pages/record-sheets/gameday-2025-01-05-voh.tsx
+++ src/pages/record-sheets/gameday-2025-01-05-voh.tsx
@@ -1,15 +1,3 @@
-import {
-  Backdrop,
-  Box,
-  SpeedDial,
-  SpeedDialAction,
-  SpeedDialIcon,
-} from "@mui/material";
-import { useState } from "react";
-import SnowshoeingIcon from "@mui/icons-material/Snowshoeing";
-import Groups2Icon from "@mui/icons-material/Groups2";
-import LocalAirportIcon from "@mui/icons-material/LocalAirport";
-import { PlaySheets } from "@/recordSheets/PlaySheets";
 import { RecordSheets } from "@/recordSheets/RecordSheets";
 
 const involvedUnits = [
