e82859d356f3150aee24fa0c07157c30de3c0f15
Wilson Mead III <kem@rangoric.com>
2025-01-19 14:36:36 -0500

    Update LocationDialog.tsx

--- src/recordSheets/LocationDialog.tsx
+++ src/recordSheets/LocationDialog.tsx
@@ -1,11 +1,4 @@
-import {
-  Dialog,
-  DialogTitle,
-  Button,
-  Box,
-  Typography,
-  ButtonGroup,
-} from "@mui/material";
+import { Dialog, DialogTitle, Button, Box, Typography, ButtonGroup } from "@mui/material";
 import { eLocations, ILocation, IRecordSheet } from "./data/IRecordSheet";
 import ReportIcon from "@mui/icons-material/Report";
 import HandymanIcon from "@mui/icons-material/Handyman";
@@ -19,12 +12,7 @@ export interface ILocationDialog {
   onClose: () => void;
 }
 
-export const LocationDialog = ({
-  onClose,
-  open,
-  unit,
-  location,
-}: ILocationDialog) => {
+export const LocationDialog = ({ onClose, open, unit, location }: ILocationDialog) => {
   const [unitStatus, setUnitStatus] = useUnitStatus(unit.pilotData.callSign);
   const unitLocation = unit.vehicle.locations[location];
   const damageAtLocation = unitStatus.locations?.[location] ?? {
@@ -46,15 +34,12 @@ export const LocationDialog = ({
       },
     });
   };
-  const critSlotAt = (critSlot: number) =>
-    damageAtLocation.critSlots.find((t) => t.critSlot === critSlot);
+  const critSlotAt = (critSlot: number) => damageAtLocation.critSlots.find((t) => t.critSlot === critSlot);
   const setNewCritSlot = (newCritSlot: ICritSlotStatus) => {
     const existingCritSlot = critSlotAt(newCritSlot.critSlot);
     const critSlot = {
       critSlot: newCritSlot.critSlot,
-      damaged: newCritSlot.damaged
-        ? !existingCritSlot?.damaged
-        : existingCritSlot?.damaged ?? false,
+      damaged: newCritSlot.damaged ? !existingCritSlot?.damaged : existingCritSlot?.damaged ?? false,
       spentAmmo: (existingCritSlot?.spentAmmo ?? 0) + newCritSlot.spentAmmo,
     };
     setUnitStatus({
@@ -63,12 +48,7 @@ export const LocationDialog = ({
         ...unitStatus.locations,
         [location]: {
           ...damageAtLocation,
-          critSlots: [
-            ...damageAtLocation.critSlots.filter(
-              (t) => t.critSlot !== newCritSlot.critSlot
-            ),
-            critSlot,
-          ],
+          critSlots: [...damageAtLocation.critSlots.filter((t) => t.critSlot !== newCritSlot.critSlot), critSlot],
         },
       },
     });
@@ -77,78 +57,38 @@ export const LocationDialog = ({
   return (
     <Dialog onClose={onClose} open={open}>
       <DialogTitle>{location}</DialogTitle>
-      <Box
-        padding={1}
-        width={360}
-        display={"flex"}
-        flexDirection={"column"}
-        gap={1}
-      >
-        <Box
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
+      <Box padding={1} width={360} display={"flex"} flexDirection={"column"} gap={1}>
+        <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
           <Typography>
-            Armour: {unitLocation.armour - damageAtLocation.armour}/
-            {unitLocation.armour}
+            Armour: {unitLocation.armour - damageAtLocation.armour}/{unitLocation.armour}
           </Typography>
           <ButtonGroup>
-            <Button
-              onClick={() =>
-                setNewValue({ armour: damageAtLocation.armour + 1 })
-              }
-            >
+            <Button onClick={() => setNewValue({ armour: damageAtLocation.armour + 1 })}>
               <ReportIcon />
             </Button>
-            <Button
-              onClick={() =>
-                setNewValue({ armour: damageAtLocation.armour - 1 })
-              }
-            >
+            <Button onClick={() => setNewValue({ armour: damageAtLocation.armour - 1 })}>
               <HandymanIcon />
             </Button>
           </ButtonGroup>
         </Box>
         {!!unitLocation.rearArmour && (
-          <Box
-            display={"flex"}
-            justifyContent={"space-between"}
-            alignItems={"center"}
-          >
+          <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
             <Typography>
-              Rear Armour:{" "}
-              {unitLocation.rearArmour - damageAtLocation.rearArmour}/
-              {unitLocation.rearArmour}
+              Rear Armour: {unitLocation.rearArmour - damageAtLocation.rearArmour}/{unitLocation.rearArmour}
             </Typography>
             <ButtonGroup>
-              <Button
-                onClick={() =>
-                  setNewValue({ rearArmour: damageAtLocation.rearArmour + 1 })
-                }
-              >
+              <Button onClick={() => setNewValue({ rearArmour: damageAtLocation.rearArmour + 1 })}>
                 <ReportIcon />
               </Button>
-              <Button
-                onClick={() =>
-                  setNewValue({ rearArmour: damageAtLocation.rearArmour - 1 })
-                }
-              >
+              <Button onClick={() => setNewValue({ rearArmour: damageAtLocation.rearArmour - 1 })}>
                 <HandymanIcon />
               </Button>
             </ButtonGroup>
           </Box>
         )}
-        <Box
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
+        <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
           <Typography>
-            Structure:{" "}
-            {unitLocation.internalStructure -
-              damageAtLocation.internalStructure}
-            /{unitLocation.internalStructure}
+            Structure: {unitLocation.internalStructure - damageAtLocation.internalStructure}/{unitLocation.internalStructure}
           </Typography>
           <ButtonGroup>
             <Button
@@ -173,38 +113,29 @@ export const LocationDialog = ({
         </Box>
         <Box>
           <Typography>
-            Crit Slots: {unitLocation.critSlots.length}/
-            {unitLocation.critSlots.length}
+            Crit Slots: {unitLocation.critSlots.length - damageAtLocation.critSlots.filter((t) => t.damaged).length}/{unitLocation.critSlots.length}
           </Typography>
           {unitLocation.critSlots.map((slot, index) => (
-            <Box
-              key={slot.item + index}
-              color={critSlotAt(index)?.damaged ? "error.main" : "text.primary"}
-              display={"flex"}
-              justifyContent={"space-between"}
-            >
-              <Typography>
+            <ButtonGroup key={slot.item + index} sx={{ width: "100%" }}>
+              <Button
+                onClick={() =>
+                  setNewCritSlot({
+                    critSlot: index,
+                    damaged: true,
+                    spentAmmo: 0,
+                  })
+                }
+                color={critSlotAt(index)?.damaged ? "error" : "primary"}
+              >
                 {(index % 6) + 1}: {slot.item}{" "}
                 {slot.ammoCount && (
                   <>
-                    {slot.ammoCount - (critSlotAt(index)?.spentAmmo ?? 0)}/
-                    {slot.ammoCount}
+                    {slot.ammoCount - (critSlotAt(index)?.spentAmmo ?? 0)}/{slot.ammoCount}
                   </>
                 )}
-              </Typography>
-              <Box>
-                <Button
-                  onClick={() =>
-                    setNewCritSlot({
-                      critSlot: index,
-                      damaged: true,
-                      spentAmmo: 0,
-                    })
-                  }
-                >
-                  <HandymanIcon />
-                </Button>
-                {slot.ammoCount && (
+              </Button>
+              {slot.ammoCount !== undefined && (
+                <>
                   <Button
                     onClick={() =>
                       setNewCritSlot({
@@ -216,8 +147,6 @@ export const LocationDialog = ({
                   >
                     Spend
                   </Button>
-                )}
-                {slot.ammoCount && (
                   <Button
                     onClick={() =>
                       setNewCritSlot({
@@ -229,9 +158,9 @@ export const LocationDialog = ({
                   >
                     Add
                   </Button>
-                )}
-              </Box>
-            </Box>
+                </>
+              )}
+            </ButtonGroup>
           ))}
         </Box>
       </Box>
