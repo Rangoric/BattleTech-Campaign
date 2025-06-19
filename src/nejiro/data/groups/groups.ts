e9c99487dc9d56e35155d40ee8cb22fae7e1da4c
Wilson Gearld Mead III <kem@rangoric.com>
2025-06-19 12:39:56 -0400

    ...

--- src/nejiro/data/groups/groups.ts
+++ src/nejiro/data/groups/groups.ts
@@ -1,15 +1,6 @@
 import { GSF } from "./GSF";
-import { Ironborn } from "./Ironborn";
 import { KokuryuKai } from "./KokuryuKai";
 import { RoadsWarriors } from "./RoadsWarriors";
 import { TheDragonsClutch } from "./TheDragonsClutch";
-import { WintersDream } from "./WintersDream";
 
-export const groups = [
-  GSF,
-  Ironborn,
-  RoadsWarriors,
-  TheDragonsClutch,
-  WintersDream,
-  KokuryuKai,
-];
+export const groups = [GSF, RoadsWarriors, TheDragonsClutch, KokuryuKai];
